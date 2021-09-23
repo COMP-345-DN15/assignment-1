bbed01d0c0730cc6dc35f0f43a2a9560117653c1
jvlyndark <50807142+jvlyndark@users.noreply.github.com>
2021-09-23 10:14:59 -0400

    create basic map classes

---
+++ MapLoader.cpp
@@ -0,0 +1,15 @@
+//
+// COMP-345 - Group DN15
+// MapLoader - Source
+
+#pragma once
+#include "MapLoader.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+MapLoader::MapLoader()
+{
+    
+}
---
+++ MapLoader.h
@@ -0,0 +1,22 @@
+//
+// COMP-345 - Group DN15
+// MapLoader - Header
+
+#pragma once
+#include "Map.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class Map;
+
+class MapLoader
+{
+    
+public:
+    
+private:
+
+};
