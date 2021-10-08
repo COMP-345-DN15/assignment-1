d28183db88e9516fc97b7b93cf8c7f1a25e7da26
Kimberley <kimberleyjean.b@gmail.com>
2021-10-08 17:47:48 -0400

    Added testCards() method to confirm run is successful

--- COMP345-A1/CardsDriver.cpp
+++ COMP345-A1/CardsDriver.cpp
@@ -11,34 +11,50 @@
  using namespace std;
 
 
+ //int main(){
+
+ //    std::cout <<"Warzome is setting up the deck"<<endl;
+ //    Deck d;
+
+ //    std::cout <<"-----------------------------------"<<endl;
+
+ //    std::cout <<"Warzome is printing the deck..."<<endl;
+ //    d.print_deck();
+ //    std::cout <<"The deck has "<< d.getsize()<<" cards."<<endl;
+ //    std::cout <<"-----------------------------------"<<endl;
+ //   
+ //    std::cout <<"-----------------------------------"<<endl;
+ //    d.draw();
+ //    std::cout <<"-----------------------------------"<<endl;
+ //    std::cout <<"The deck has "<< d.getsize()<<" cards."<<endl;
+ //    std::cout <<"-----------------------------------"<<endl;
+ //    std::cout <<"Printing new Deck.........."<<endl;
+ //    d.print_deck();
 
 
+ //    return 0;
 
- int main(){
+ //};
 
-     std::cout <<"Warzome is setting up the deck"<<endl;
+
+ void testCards() {
+ 
+     std::cout << "Warzome is setting up the deck" << endl;
      Deck d;
 
-     std::cout <<"-----------------------------------"<<endl;
+     std::cout << "-----------------------------------" << endl;
 
-     std::cout <<"Warzome is printing the deck..."<<endl;
+     std::cout << "Warzome is printing the deck..." << endl;
      d.print_deck();
-     std::cout <<"The deck has "<< d.getsize()<<" cards."<<endl;
-     std::cout <<"-----------------------------------"<<endl;
-    
-     std::cout <<"-----------------------------------"<<endl;
+     std::cout << "The deck has " << d.getsize() << " cards." << endl;
+     std::cout << "-----------------------------------" << endl;
+
+     std::cout << "-----------------------------------" << endl;
      d.draw();
-     std::cout <<"-----------------------------------"<<endl;
-     std::cout <<"The deck has "<< d.getsize()<<" cards."<<endl;
-     std::cout <<"-----------------------------------"<<endl;
-     std::cout <<"Printing new Deck.........."<<endl;
+     std::cout << "-----------------------------------" << endl;
+     std::cout << "The deck has " << d.getsize() << " cards." << endl;
+     std::cout << "-----------------------------------" << endl;
+     std::cout << "Printing new Deck.........." << endl;
      d.print_deck();
 
-
-
-
-     return 0;
-
- };
-
-
+ }
--- COMP345-A1/Driver.cpp
+++ COMP345-A1/Driver.cpp
@@ -1,13 +1,16 @@
 #include "GameEngineDriver.h"
 #include "OrdersDriver.h"
 #include "PlayerDriver.h"
+#include "CardsDriver.h"
 
 int main() {
 
 
-	//testGameEngine();
-	OrdersTest();
-	//testPlayer();
+	//testGameEngine(); // uncomment to run GameEngine class
+	//OrdersTest(); // uncomment to run Orders class
+	//testPlayer(); // uncomment to run Player class
+	testCards(); // uncommment to run Cards class
+
 
 	return 0;
 }
