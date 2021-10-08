70a0dcdefbfad00e1954afb6be446b84bb94d980
ahmedshawn33 <30379259+ahmedshawn33@users.noreply.github.com>
2021-10-07 22:11:47 -0400

    Update Cards.h

--- COMP345-A1/Cards.h
+++ COMP345-A1/Cards.h
@@ -1,5 +1,10 @@
 #pragma once
+#include<iostream>
 #include <vector>
+#include <ctime>
+#include <string>
+#include <stdlib.h>     /* srand, rand */
+
 
 using namespace std;
 
@@ -7,34 +12,43 @@ class Cards {
 
 	// could have enum values for the card types
 	enum cardType { bomb, reinforcement, blockade, airlift, diplomacy };
+	cardType ctype;
+
+	public:
+	//special order played by the player and sent back to the deck once it is played
+		Cards();
+		string display_card();
+		Cards(int x);
+		void play();
 
 
 };
 
 class Deck {
 
-public:
-
-	
-
 private:
 
 	// vector that contains a list of Card objects
-	vector<Cards*> cards;
+	Cards card;
+	vector<Cards> cards;
+	
 
-	Cards* draw();
-	void returnToDeck(Cards);
+public:
+	Deck();
+	void add_to_deck(Cards c);
+	void print_deck();
+	void draw();
+ 	int getsize();	
+	void shuffle_deck();
 
 };
 
 
 class Hand {
-
 public:
 
 private:
-
 	// vector that contains a list of Card objects
-	vector<Cards*> cards;
+	vector<Cards> cards;
 
 };
